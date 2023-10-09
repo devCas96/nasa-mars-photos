feb7b365873f0cd0ccf1fa3508847420321694a4
Sebastián Castaño <sebas.cas96@hotmail.com>
2023-10-09 13:09:23 -0500

    fix: Removed unnecessary URL object in BASE URL.

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,4 +1,2 @@
 export const API_KEY = 'iHRDIGDqSrUX1KRpEx1UfOyLNTTalujMhOnggZIt';
-export const BASE_URL = new URL(
-  'https://api.nasa.gov/mars-photos/api/v1/rovers/'
-);
+export const BASE_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/';
