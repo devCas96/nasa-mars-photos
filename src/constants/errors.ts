4abfca5d7df6258f2919f3ff0f158a601f1bfdf8
Sebastián Castaño <sebas.cas96@hotmail.com>
2023-10-11 23:25:19 -0500

    feat: Constants update, now there are a file for handle front and backend error messages and camera types enum added.

---
+++ src/constants/errors.ts
@@ -0,0 +1,10 @@
+export enum FrontErrors {
+  EMPTY_RESULT = 'Ups! No results found with those search params.'
+}
+
+export enum BackErrors {
+  WRONG_PARAMS = 'Invalid request parameters.',
+  SERVER_ERROR = 'Something went wrong connecting with NASA API.',
+  UNKNOW_ERROR = 'An unknown error occurred.',
+
+}
--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,4 +1,4 @@
-export const API_KEY: string = 'iHRDIGDqSrUX1KRpEx1UfOyLNTTalujMhOnggZIt';
+export const API_KEY: string = 'd9N6IDMllxviYXUAiNLXkX0S4oGaFBq3kzDDd6EI';
 export const BASE_URL: string = 'https://api.nasa.gov/mars-photos/api/v1/rovers/';
 export const BASE_APP_URL: string = 'http://localhost:3000/';
 export const PHOTOS_PER_PAGE: number = 25;
